3f805178cee0561002ca3f11375804f53e6a891d
Janhavi-Khairnar <khairnarj2000@gmail.com>
2023-07-19 00:41:00 +0530

    [ADDED]:[Janhavi] Creating Push Function in Stack Project

---
+++ src/com/technospace/stackproject/StackClass.java
@@ -0,0 +1,33 @@
+package com.technospace.stackproject;
+
+import java.util.Scanner;
+
+public class StackClass {
+
+	int[] stack = new int[5];
+	int top=-1;
+	
+	Scanner sc = new Scanner(System.in);
+	
+	public void push() {
+		if(top == stack.length-1) {
+			System.out.println("Stack is overflow.");
+		} else{
+			System.out.println("Enter the Number :");
+			int num = sc.nextInt();
+			top++;
+			stack[top] = num;
+		}
+	}
+	
+	public static void main(String[] args) {
+		StackClass stackClass = new StackClass();
+		stackClass.push();
+		stackClass.push();
+		stackClass.push();
+		stackClass.push();
+		stackClass.push();
+		stackClass.push();
+	}
+
+}
